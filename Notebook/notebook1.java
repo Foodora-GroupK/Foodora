0cf4bcffc8729270649841f570176186907f63fb
uijinlee-luj <uijin.lee@student-cs.fr>
2025-05-19 16:52:15 +0200

    add

--- Notebook/notebook1.java
+++ Notebook/notebook1.java
@@ -1,1 +1,9 @@
-//java notebook
+public class restaurant {
+    private String name;
+    private String surname;
+    private int uniqueId;
+    private String address;
+    private String passward;
+    private Coordinate location;
+
+}
---
+++ Notebook/notebook2.java
@@ -0,0 +1,29 @@
+public class Coordinate{
+    private double x;
+    private double y;
+
+    public Coordinate(double x, double y){
+        this.x=x;
+        this.y=y;
+    }
+
+    public double getx(){
+        return x;
+    }
+
+    public double gety(){
+        return y;
+    }
+}
+
+//ddddd
+
+public class restaurant {
+    private String name;
+    private int uniqueId;
+    private String username;
+    private String passward;
+    private Coordinate location;
+
+}
+
