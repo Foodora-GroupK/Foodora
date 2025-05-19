6b74dd2d8e6751d22619056a446f7fd89efda6b1
chaeyeon <lcy321624@gmail.com>
2025-05-19 17:01:55 +0200

    modified notebook2

--- Notebook/notebook2.java
+++ Notebook/notebook2.java
@@ -1,22 +1,47 @@
-public class Coordinate{
+public class Menu {
+    public enum Category { STARTER, MAIN_DISH, DESSERT}
+    public enum Type { STANDARD, VEGETERIAN}
+
+    private String name;
+    private double pricel
+    private Category category;
+    private Type type;
+    private boolean isGlutenFree;
+    
+    public Menu(String name, double price, Category category, Type type, boolean is GlutenFree) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.type = type;
+        this.isGlutenFree = isGlutenFree;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
+
+
+
+
+public class Coordinate {
     private double x;
     private double y;
 
-    public Coordinate(double x, double y){
+    public Coordinate(double x, double y) {
         this.x=x;
         this.y=y;
     }
 
-    public double getx(){
+    public double getX() {
         return x;
     }
 
-    public double gety(){
+    public double getY() {
         return y;
     }
 }
-
-//ddddd
 
 public class restaurant {
     private String name;
