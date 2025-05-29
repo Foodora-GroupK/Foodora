680ea34c10c16009bf26e0d9fd771575f87d310d
chaeyeon <lcy321624@gmail.com>
2025-05-29 14:54:22 +0200

    remove notebook 1

--- Notebook/notebook1.java
+++
@@ -1,9 +0,0 @@
-public class restaurant {
-    private String name;
-    private String surname;
-    private int uniqueId;
-    private String address;
-    private String passward;
-    private Coordinate location;
-
-}
