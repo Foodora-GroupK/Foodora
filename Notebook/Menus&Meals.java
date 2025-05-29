e6714f4e5edc2aa6fd0f6cd4551f8cc67a5539f8
chaeyeon <lcy321624@gmail.com>
2025-05-29 14:20:09 +0200

    update Menus&Meals

--- Notebook/Menus&Meals.java
+++ Notebook/Menus&Meals.java
@@ -1,3 +1,6 @@
+import jave.util.List;
+import java.util.ArrayList;
+
 public class MenuItem {
     public enum Category {STARTER, MAIN_DISH, DESSERT}
     public enum Type {STANDARD, VEGETARIAN}
@@ -34,7 +37,45 @@ public class MenuItem {
 
 }
 
-import jave.util.List;
+public class Menu {
+    private List<MenuItem> starters;
+    private List<MenuItem> mainDishes;
+    private List<MenuItem> desserts;
+
+    public Menu() {
+        this.starters = new ArrayList<>();
+        this.mainDishes = new ArrayList<>();
+        this.desserts = new ArrayList<>();
+    }
+
+    public void addItem(MenuItem item) {
+        switch (item.getCategory()) {
+            case STARTER -> starters.add(item);
+            case MAIN_DISH -> mainDishes.add(item);
+            case DESSERT -> desserts.add(item);
+        }
+    }
+
+    public void removeItem(MenuItem item) {
+        switch (item.getCategory()) {
+            case STARTER -> starters.remove(item);
+            case MAIN_DISH -> mainDishes.remove(item);
+            case DESSERT -> desserts.remove(item);
+        }
+    }
+
+    public List<MenuItem> getStarters() {
+        return starters;
+    }
+
+    public List<MenuItem> getMainDishes() {
+        return mainDishes;
+    }
+
+    public List<MenuItem> getDesserts() {
+        return desserts;
+    }
+}
 
 public class Meal {
     public enum MealType {STANDARD, VEGETARIAN, GLUTEN_FREE}
