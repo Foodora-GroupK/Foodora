14f9841cea9be094cee08e041e348c35207ff81b
chaeyeon302 <111568520+chaeyeon302@users.noreply.github.com>
2025-06-01 18:53:12 +0200

    Delete Notebook/fidelity/BasicFidelityCard.java

--- Notebook/fidelity/BasicFidelityCard.java
+++
@@ -1,6 +0,0 @@
-public class BasicFidelityCard implements FidelityCard {
-    @Override
-    public double applyDiscount(double totalPrice, Customer customer) {
-        return totalPrice;
-    }
-}
